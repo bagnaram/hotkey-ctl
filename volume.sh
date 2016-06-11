20e7a47aebc63da2f9ce78f32eaf15d6581c977b
Matt Bagnara <bagnaramatt@gmail.com>
2016-06-10 20:42:08 -0400

    split logic for pulse and ALSA

--- volume.sh
+++ volume.sh
@@ -1,16 +1,54 @@
 #!/bin/bash
 
 cd ~/hotkey-ctl
-if [ "$1" = "up" ]
+
+while [[ $# > 0 ]]
+do
+key="$1"
+
+case $key in
+    pony)
+    PULSE=true
+    ;;
+    up)
+    DIR="up"
+    ;;
+    down)
+    DIR="down"
+    ;;
+    --default)
+    DEFAULT=YES
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift
+done
+
+
+if [ "${DIR}" = "up" ]
 then
-  amixer set Master 5%+ unmute
-  myvolume=`amixer get 'Master',0 | gawk 'match($0, /\[([0-9]*)%\]/, m) {print m[1]; exit;}'`
-  notify-send " " -i /usr/share/icons/gnome/32x32/apps/multimedia-volume-control.png -h int:value:$myvolume -h string:x-canonical-private-synchronous:myvolume &
+  if [ "$PULSE" = true ]
+  then
+    ponymix -c broadwell-rt286 increase 5% 
+    myvolume=`ponymix -c broadwell-rt286 get-volume`
+  else
+    amixer set Master 5%+ unmute
+    myvolume=`amixer get 'Master',0 | gawk 'match($0, /\[([0-9]*)%\]/, m) {print m[1]; exit;}'`
+  fi
+  notify-send " " -i /usr/share/icons/gnome/48x48/apps/multimedia-volume-control.png -h int:value:$myvolume -h string:x-canonical-private-synchronous:myvolume &
   aplay ./pop.wav
-elif [ "$1" = "down" ]
+elif [ "${DIR}" = "down" ]
 then
-  amixer set Master 5%- unmute
-  myvolume=`amixer get 'Master',0 | gawk 'match($0, /\[([0-9]*)%\]/, m) {print m[1]; exit;}'`
-  notify-send " " -i /usr/share/icons/gnome/32x32/apps/multimedia-volume-control.png -h int:value:$myvolume -h string:x-canonical-private-synchronous:volume &
+  if [ "$PULSE" = true ]
+  then
+    ponymix -c broadwell-rt286 decrease 5% 
+    myvolume=`ponymix -c broadwell-rt286 get-volume`
+  else
+    amixer set Master 5%- unmute
+    myvolume=`amixer get 'Master',0 | gawk 'match($0, /\[([0-9]*)%\]/, m) {print m[1]; exit;}'`
+  fi
+  notify-send " " -i /usr/share/icons/gnome/48x48/apps/multimedia-volume-control.png -h int:value:$myvolume -h string:x-canonical-private-synchronous:volume &
   aplay ./pop.wav
 fi
