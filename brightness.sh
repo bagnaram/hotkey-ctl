a6676e02460a1925e10b0f732f6475a74ec28dd0
Matt Bagnara <mbagnara@redhat.com>
2017-11-15 11:49:52 -0500

    Converted to using dunst for notification management. Switched to ascii based progress. 	modified:   brightness.sh Switched to ascii based progress. 	modified:   volume.sh Switched to ascii based progress.

--- brightness.sh
+++ brightness.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-mybrightness=$(( `cat /sys/class/backlight/intel_backlight/brightness` * 100 / 1000 ))
+mybrightness=$(( `cat /sys/class/backlight/intel_backlight/brightness` * 100 / `cat /sys/class/backlight/intel_backlight/max_brightness` ))
+
+  progress=""
+k=$(( $(( $mybrightness + 10 )) / 10))
+    progress="["
+    for ((i = 0 ; i <= k; i++)); do progress="$progress#"; done
+    for ((j = i ; j <= 10 ; j++)); do progress="$progress "; done
+    progress="$progress] "
 
 if [ "$1" = "up" ]
 then
-  xbacklight +10
-  notify-send " " -i notification-display-brightness-low -h int:value:$(( $mybrightness + 10 )) -h string:x-canonical-private-synchronous:brightness &
+  light -A 10
+
+
+
+  notify-send "Brightness: %p\n$progress" -i /usr/share/icons/HighContrast/48x48/status/display-brightness.png -h int:value:$(( $mybrightness + 10 )) -h string:x-canonical-private-synchronous:brightness &
 elif [ "$1" = "down" ]
 then
-  xbacklight -10
-  notify-send " " -i notification-display-brightness-low -h int:value:$(( $mybrightness - 10 )) -h string:x-canonical-private-synchronous:brightness &
+  light -U 10
+  notify-send "Brightness: %p\n$progress" -i /usr/share/icons/HighContrast/48x48/status/display-brightness.png -h int:value:$(( $mybrightness - 10 )) -h string:x-canonical-private-synchronous:brightness &
 else
   echo "Expecting control arguments \"up\" or \"down\"."
 fi
--- volume.sh
+++ volume.sh
@@ -2,6 +2,17 @@
 
 cd ~/hotkey-ctl
 
+percentage() {
+
+  progress=""
+  k=$(( $myvolume / 10))
+  progress="["
+  for ((i = 0 ; i <= k; i++)); do progress="$progress#"; done
+  for ((j = i ; j <= 10 ; j++)); do progress="$progress "; done
+  progress="$progress] "
+
+}
+
 while [[ $# > 0 ]]
 do
 key="$1"
@@ -33,11 +44,14 @@ then
   then
     ponymix -c broadwell-rt286 increase 5% 
     myvolume=`ponymix -c broadwell-rt286 get-volume`
+    percentage
+
   else
     amixer set Master 5%+ unmute
     myvolume=`amixer get 'Master',0 | gawk 'match($0, /\[([0-9]*)%\]/, m) {print m[1]; exit;}'`
+    percentage
   fi
-  notify-send " " -i /usr/share/icons/gnome/48x48/apps/multimedia-volume-control.png -h int:value:$myvolume -h string:x-canonical-private-synchronous:myvolume &
+  notify-send "Volume %p\n$progress" -i /usr/share/icons/gnome/48x48/apps/multimedia-volume-control.png -h int:value:$myvolume -h string:x-canonical-private-synchronous:myvolume &
   aplay ./pop.wav
 elif [ "${DIR}" = "down" ]
 then
@@ -45,10 +59,13 @@ then
   then
     ponymix -c broadwell-rt286 decrease 5% 
     myvolume=`ponymix -c broadwell-rt286 get-volume`
+    percentage
   else
     amixer set Master 5%- unmute
     myvolume=`amixer get 'Master',0 | gawk 'match($0, /\[([0-9]*)%\]/, m) {print m[1]; exit;}'`
+    percentage
   fi
-  notify-send " " -i /usr/share/icons/gnome/48x48/apps/multimedia-volume-control.png -h int:value:$myvolume -h string:x-canonical-private-synchronous:volume &
+  notify-send "Volume: %p\n$progress" -i /usr/share/icons/gnome/48x48/apps/multimedia-volume-control.png -h int:value:$myvolume -h string:x-canonical-private-synchronous:volume &
   aplay ./pop.wav
 fi
+
